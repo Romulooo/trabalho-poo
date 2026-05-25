18e57e5e9d1b2b99266619e3f4786b6ce9f64589
Rômulo <romulonilsonhorn@gmail.com>
2026-05-25 16:35:17 -0300

    Criação do player, movimentação e animação

--- Player/PasicMovement.gd
+++ Player/PasicMovement.gd
@@ -1,25 +1,37 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 500.0
+const JUMP_VELOCITY = -600.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	
+	
+	if not is_on_floor():	
+		velocity += get_gravity() * delta * 1.2
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("p1jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("p1left", "p1right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
+	if is_on_floor():
+		if direction == 0:
+			$AnimatedSprite2D.play("idle")
+		else:
+			$AnimatedSprite2D.play("run")
+	else:
+		$AnimatedSprite2D.play("run")
+		
+	if direction > 0:
+		$AnimatedSprite2D.flip_h = false
+	if direction < 0:
+		$AnimatedSprite2D.flip_h = true
+		
+	
 	move_and_slide()
